b802ac22ac13d926a417706695e53f2322a88778
Mauricio Souza <mauricio.sousa@dextra-sw.com.br>
2020-03-26 19:40:02 -0300

    enviroment class added

---
+++ src/dist/common/enviroment.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.enviroment = {
+    server: {
+        port: process.env.SERVER_PORT || 3333
+    }
+};
+//# sourceMappingURL=enviroment.js.map

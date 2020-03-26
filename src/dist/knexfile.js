db999282c88072f402054705f153b95416cead1f
Mauricio Souza <mauricio.sousa@dextra-sw.com.br>
2020-03-26 18:03:34 -0300

    remove directories duplicateds

--- src/dist/knexfile.js
+++ src/dist/knexfile.js
@@ -1,44 +1,46 @@
 // Update with your config settings.
 module.exports = {
     development: {
-        client: 'sqlite3',
-        connection: {
-            filename: './src/db.sqlite'
-        },
-        migrations: {
-            directory: './src/database/migrations'
-        },
-        useNullAsDefault: true
+      client: 'sqlite3',
+      connection: {
+        filename: './src/db.sqlite'
+      },
+      migrations: { 
+        directory: './src/database/migrations'
+      },
+      useNullAsDefault: true
     },
+  
     staging: {
-        client: 'postgresql',
-        connection: {
-            database: 'my_db',
-            user: 'username',
-            password: 'password'
-        },
-        pool: {
-            min: 2,
-            max: 10
-        },
-        migrations: {
-            tableName: 'knex_migrations'
-        }
+      client: 'postgresql',
+      connection: {
+        database: 'my_db',
+        user:     'username',
+        password: 'password'
+      },
+      pool: {
+        min: 2,
+        max: 10
+      },
+      migrations: {
+        tableName: 'knex_migrations'
+      }
     },
+  
     production: {
-        client: 'postgresql',
-        connection: {
-            database: 'my_db',
-            user: 'username',
-            password: 'password'
-        },
-        pool: {
-            min: 2,
-            max: 10
-        },
-        migrations: {
-            tableName: 'knex_migrations'
-        }
+      client: 'postgresql',
+      connection: {
+        database: 'my_db',
+        user:     'username',
+        password: 'password'
+      },
+      pool: {
+        min: 2,
+        max: 10
+      },
+      migrations: {
+        tableName: 'knex_migrations'
+      }
     }
-};
-//# sourceMappingURL=knexfile.js.map
+  
+  };  
--- src/dist/src/database/connection.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const knex = require("knex");
-const configuration = require("../../knexfile");
-exports.connection = knex(configuration.development);
-//# sourceMappingURL=connection.js.map
